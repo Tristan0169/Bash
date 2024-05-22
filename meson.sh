4f7782605824cba7984043eea13d73db49354a27
Tristan0169 <167325260+Tristan0169@users.noreply.github.com>
2024-05-23 05:25:59 +0800

    Update and rename Meson.sh to meson.sh

--- meson.sh
+++ meson.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+set -x
 
-# Download and extract the apphub-linux-amd64.tar.gz
+echo "Downloading and extracting meson_cdn..."
 wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.20/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
 
-# Change directory to apphub-linux-amd64
+echo "Configuring meson_cdn..."
 cd ./meson_cdn-linux-amd64
-
-# Configure meson_cdn
 sudo ./meson_cdn config set --token=flpzbbwkogddyofpab335c975434d950 --https_port=443 --cache.size=30
 
-# Start the meson_cdn service
+echo "Starting meson_cdn service..."
 sudo ./service start meson_cdn
