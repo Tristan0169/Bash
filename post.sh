c0157eaef0c1a85a66b734f10db902f69cb2b5f5
Tristan0169 <167325260+Tristan0169@users.noreply.github.com>
2024-05-23 20:07:46 +0800

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 set -x
+
 # Download and extract the apphub-linux-amd64.tar.gz
 curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz
 
